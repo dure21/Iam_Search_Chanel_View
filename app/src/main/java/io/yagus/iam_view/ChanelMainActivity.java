d2d58777eeaaaafd556117f1d67cac8f7f604eed
KJH <jinhungkim@hanmail.net>
2020-03-27 16:49:41 +0900

    NEW

--- app/src/main/java/io/yagus/iam_view/ChanelMainActivity.java
+++ app/src/main/java/io/yagus/iam_view/ChanelMainActivity.java
@@ -2,13 +2,27 @@ package io.yagus.iam_view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import io.yagus.iam_view.databinding.ActivityChanelMainBinding;
+import io.yagus.iam_view.databinding.ActivityCreateChannelBinding;
 
 public class ChanelMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chanel_main);
+        ActivityChanelMainBinding binding = ActivityChanelMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btChanelUserList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChanelUserListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/io/yagus/iam_view/ChanelUserListActivity.java
@@ -0,0 +1,14 @@
+package io.yagus.iam_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChanelUserListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chanel_user_list);
+    }
+}
